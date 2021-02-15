d900ab6c8317537d260c308fce8f6be61e6a73c0
WojtekSJ <wojnad@o2.pl>
2021-02-15 17:43:17 +0100

    14.1 Zadanie 1 poprawione

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
-    String username;
+    public String username;
     public ForumUser() {
         this.username = "John Smith";
     }
